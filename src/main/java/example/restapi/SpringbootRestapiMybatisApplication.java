3ec80e345a3902551f0bb0c35f6104b8accd64dc
junglestory <monocaine@gmail.com>
2023-06-07 22:46:41 +0900

    first commit

---
+++ src/main/java/example/restapi/SpringbootRestapiMybatisApplication.java
@@ -0,0 +1,13 @@
+package example.restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestapiMybatisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRestapiMybatisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/example/restapi/SpringbootRestapiMybatisApplicationTests.java
@@ -0,0 +1,13 @@
+package example.restapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRestapiMybatisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
